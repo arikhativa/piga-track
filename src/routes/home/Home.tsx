1e2fb3e5c5dc5fbd62818100bbd9d9b2ca04ec68
arikhativa <arikhativa@gmail.com>
2026-09-08 22:49:46 +0300

    feat: added icons to home

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -1,5 +1,6 @@
 import { Plus } from "lucide-react";
 import { Link } from "react-router-dom";
+import GlareHover from "#/components/GlareHover";
 import PixelIcon from "#/components/icon/lord pigafetta";
 import { Card, CardContent } from "#/components/ui/card";
 import { Button } from "@/components/ui/button";
@@ -7,20 +8,35 @@ import { Button } from "@/components/ui/button";
 export function Home() {
 	return (
 		<div className="flex flex-col h-full">
-			<div className="flex justify-center items-center flex-1 ">
-				<Card className="bg-accent border-2">
-					<CardContent>
-						<PixelIcon className="size-40" />
-					</CardContent>
-				</Card>
+			<div className="flex justify-center items-center flex-1">
+				<GlareHover
+					width="auto"
+					height="auto"
+					background="transparent"
+					borderColor="transparent"
+					borderRadius="0.5rem"
+					glareColor="var(--glare-color)"
+					glareOpacity={0.25}
+					glareAngle={-30}
+					glareSize={250}
+					transitionDuration={800}
+				>
+					<Card className="bg-accent border-2">
+						<CardContent>
+							<PixelIcon className="size-40" />
+						</CardContent>
+					</Card>
+				</GlareHover>
 			</div>
+
 			<div className="flex justify-center items-center">
 				<Button render={<Link to="/transaction/create" />}>
 					<Plus />
 					<span>New Transaction</span>
 				</Button>
 			</div>
-			<div className="flex-1"></div>
+
+			<div className="flex-1" />
 		</div>
 	);
 }
